0db8c88044623ae5d61a4f5381d05faf49e8dadc
wawahyu <wahyuwahyudin443@gmail.com>
2023-01-05 16:42:14 +0700

    [Permission] Internet

--- lib/resources/middlewares/product_middleware.dart
+++ lib/resources/middlewares/product_middleware.dart
@@ -1,20 +1,14 @@
 import 'dart:convert';
 
-import 'package:ppob_app/commons/credentials/mp_credential.dart';
 import 'package:ppob_app/resources/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class ProductMiddleware {
   static Future<List<ProductModel>> getAll() async {
     List<ProductModel> result = [];
-    Map<String, dynamic> body = {
-      'commands': 'pricelist',
-      'username': MPCredential.username,
-      'sign': MPCredential.sign,
-    };
-    http.Response response = await http.post(
-        Uri.parse('https://testprepaid.mobilepulsa.net/v1/legacy/index'),
-        body: jsonEncode(body));
+
+    http.Response response = await http.get(Uri.parse(
+        'https://us-central1-lamun-my-id.cloudfunctions.net/development-pricelist-mobilepulsa'));
     if (response.statusCode != 200) {
       throw 'Gagal terhbung ke server';
     }
